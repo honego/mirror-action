d869fff81f740bc051c20068a5168482631db18f
havario <157877551+havario@users.noreply.github.com>
2026-01-16 23:06:48 +0800

    upgrade
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,62 +1,69 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: Apache-2.0
 
 set -eE
 
-if [ "$DEBUG" -eq "true" ]; then
+if [ "$DEBUG" = "true" ]; then
     set -x
 fi
 
-git config --global --add safe.directory /github/workspace
+# Trust the workspace
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
 
-GIT_USERNAME=${INPUT_GIT_USERNAME:-${GIT_USERNAME:-"git"}}
-REMOTE=${INPUT_REMOTE:-"$*"}
-REMOTE_NAME=${INPUT_REMOTE_NAME:-"mirror"}
-GIT_SSH_PRIVATE_KEY=${INPUT_GIT_SSH_PRIVATE_KEY}
-GIT_SSH_PUBLIC_KEY=${INPUT_GIT_SSH_PUBLIC_KEY}
-GIT_REF=${INPUT_GIT_REF}
-GIT_PUSH_ARGS=${INPUT_GIT_PUSH_ARGS:-"--tags --force --prune"}
-GIT_SSH_NO_VERIFY_HOST=${INPUT_GIT_SSH_NO_VERIFY_HOST}
-GIT_SSH_KNOWN_HOSTS=${INPUT_GIT_SSH_KNOWN_HOSTS}
-HAS_CHECKED_OUT="$(git rev-parse --is-inside-work-tree 2> /dev/null || /bin/true)"
+# Default variable values
+GIT_USERNAME="${INPUT_GIT_USERNAME:-${GIT_USERNAME:-"git"}}"
+REMOTE="${INPUT_REMOTE:-"$*"}"
+REMOTE_NAME="${INPUT_REMOTE_NAME:-"mirror"}"
+GIT_SSH_PRIVATE_KEY="$INPUT_GIT_SSH_PRIVATE_KEY"
+GIT_SSH_PUBLIC_KEY="$INPUT_GIT_SSH_PUBLIC_KEY"
+GIT_REF="$INPUT_GIT_REF"
+GIT_PUSH_ARGS="${INPUT_GIT_PUSH_ARGS:-"--tags --force --prune"}"
+GIT_SSH_NO_VERIFY_HOST="$INPUT_GIT_SSH_NO_VERIFY_HOST"
+GIT_SSH_KNOWN_HOSTS="$INPUT_GIT_SSH_KNOWN_HOSTS"
+HAS_CHECKED_OUT="$(git rev-parse --is-inside-work-tree 2> /dev/null || true)"
 
-if [[ "${HAS_CHECKED_OUT}" != "true" ]]; then
-    echo "WARNING: repo not checked out; attempting checkout" > /dev/stderr
-    echo "WARNING: this may result in missing commits in the remote mirror" > /dev/stderr
-    echo "WARNING: this behavior is deprecated and will be removed in a future release" > /dev/stderr
-    echo "WARNING: to remove this warning add the following to your yml job steps:" > /dev/stderr
-    echo " - uses: actions/checkout@v3" > /dev/stderr
-    if [[ "${SRC_REPO}" -eq "" ]]; then
-        echo "WARNING: SRC_REPO env variable not defined" > /dev/stderr
-        SRC_REPO="https://github.com/${GITHUB_REPOSITORY}.git" > /dev/stderr
-        echo "Assuming source repo is ${SRC_REPO}" > /dev/stderr
+if [ "$HAS_CHECKED_OUT" != "true" ]; then
+    tee >&2 <<- 'EOF'
+    WARNING: repo not checked out; attempting checkout
+    WARNING: this may result in missing commits in the remote mirror
+    WARNING: this behavior is deprecated and will be removed in a future release
+EOF
+    if [ "$SRC_REPO" = "" ]; then
+        SRC_REPO="https://github.com/$GITHUB_REPOSITORY.git"
+        tee >&2 <<- EOF
+        WARNING: SRC_REPO env variable not defined
+        Assuming source repo is $SRC_REPO"
+EOF
     fi
-    git init > /dev/null
-    git remote add origin "${SRC_REPO}"
+    git init 1> /dev/null
+    git remote add origin "$SRC_REPO"
     git fetch --all > /dev/null 2>&1
 fi
 
-git config --global credential.username "${GIT_USERNAME}"
+git config --global credential.username "$GIT_USERNAME"
 
-if [[ "${GIT_SSH_PRIVATE_KEY}" != "" ]]; then
-    mkdir -p ~/.ssh
-    chmod 700 ~/.ssh
-    echo "${GIT_SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa
-    if [[ "${GIT_SSH_PUBLIC_KEY}" != "" ]]; then
-        echo "${GIT_SSH_PUBLIC_KEY}" > ~/.ssh/id_rsa.pub
-        chmod 600 ~/.ssh/id_rsa.pub
+if [ "$GIT_SSH_PRIVATE_KEY" != "" ]; then
+    mkdir -p "$HOME/.ssh"
+    chmod 700 "$HOME/.ssh"
+    echo "$GIT_SSH_PRIVATE_KEY" > "$HOME/.ssh/id_rsa"
+    if [ "$GIT_SSH_PUBLIC_KEY" != "" ]; then
+        echo "$GIT_SSH_PUBLIC_KEY" > "$HOME/.ssh/id_rsa.pub"
+        chmod 600 "$HOME/.ssh/id_rsa.pub"
     fi
-    chmod 600 ~/.ssh/id_rsa
-    if [[ "${GIT_SSH_KNOWN_HOSTS}" != "" ]]; then
-        echo "${GIT_SSH_KNOWN_HOSTS}" > ~/.ssh/known_hosts
-        git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=~/.ssh/known_hosts"
+    chmod 600 "$HOME/.ssh/id_rsa"
+    if [ "$GIT_SSH_KNOWN_HOSTS" != "" ]; then
+        echo "$GIT_SSH_KNOWN_HOSTS" > "$HOME/.ssh/known_hosts"
+        git config --global core.sshCommand "ssh -i $HOME/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=$HOME/.ssh/known_hosts"
     else
-        if [[ "${GIT_SSH_NO_VERIFY_HOST}" != "true" ]]; then
-            echo "WARNING: no known_hosts set and host verification is enabled (the default)"
-            echo "WARNING: this job will fail due to host verification issues"
-            echo "Please either provide the GIT_SSH_KNOWN_HOSTS or GIT_SSH_NO_VERIFY_HOST inputs"
+        if [ "$GIT_SSH_NO_VERIFY_HOST" != "true" ]; then
+            tee >&2 <<- 'EOF'
+            WARNING: no known_hosts set and host verification is enabled (the default)
+            WARNING: this job will fail due to host verification issues
+            Please either provide the GIT_SSH_KNOWN_HOSTS or GIT_SSH_NO_VERIFY_HOST inputs
+EOF
             exit 1
         else
-            git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+            git config --global core.sshCommand "ssh -i $HOME/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
         fi
     fi
 else
@@ -64,14 +71,16 @@ else
     git config --global credential.helper cache
 fi
 
-git remote add ${REMOTE_NAME} "${REMOTE}"
-if [[ "${INPUT_PUSH_ALL_REFS}" != "false" ]]; then
-    eval git push ${GIT_PUSH_ARGS} ${REMOTE_NAME} "\"refs/remotes/origin/*:refs/heads/*\""
+git remote add "$REMOTE_NAME" "$REMOTE"
+if [ "$INPUT_PUSH_ALL_REFS" != "false" ]; then
+    eval git push "$GIT_PUSH_ARGS" "$REMOTE_NAME" "\"refs/remotes/origin/*:refs/heads/*\""
 else
-    if [[ "${HAS_CHECKED_OUT}" != "true" ]]; then
-        echo "FATAL: You must upgrade to using actions inputs instead of args: to push a single branch" > /dev/stderr
+    if [ "$HAS_CHECKED_OUT" != "true" ]; then
+        tee >&2 <<- 'EOF'
+        echo "FATAL: You must upgrade to using actions inputs instead of args: to push a single branch"
+EOF
         exit 1
     else
-        eval git push -u ${GIT_PUSH_ARGS} ${REMOTE_NAME} "${GIT_REF}"
+        eval git push -u "$GIT_PUSH_ARGS" "$REMOTE_NAME" "$GIT_REF"
     fi
 fi
