0433a148635386cea4ab13a5734885e6ba022b7f
havario <157877551+havario@users.noreply.github.com>
2026-01-16 23:42:45 +0800

    chore: remove redundant if-else nesting
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,17 +23,13 @@ GIT_SSH_KNOWN_HOSTS="$INPUT_GIT_SSH_KNOWN_HOSTS"
 HAS_CHECKED_OUT="$(git rev-parse --is-inside-work-tree 2> /dev/null || true)"
 
 if [ "$HAS_CHECKED_OUT" != "true" ]; then
-    tee >&2 <<- 'EOF'
-    WARNING: repo not checked out; attempting checkout
-    WARNING: this may result in missing commits in the remote mirror
-    WARNING: this behavior is deprecated and will be removed in a future release
-EOF
+    echo >&2 "WARNING: repo not checked out; attempting checkout"
+    echo >&2 "WARNING: this may result in missing commits in the remote mirror"
+    echo >&2 "WARNING: this behavior is deprecated and will be removed in a future release"
     if [ "$SRC_REPO" = "" ]; then
         SRC_REPO="https://github.com/$GITHUB_REPOSITORY.git"
-        tee >&2 <<- EOF
-        WARNING: SRC_REPO env variable not defined
-        Assuming source repo is $SRC_REPO"
-EOF
+        echo >&2 "WARNING: SRC_REPO env variable not defined"
+        echo >&2 "Assuming source repo is $SRC_REPO"
     fi
     git init 1> /dev/null
     git remote add origin "$SRC_REPO"
@@ -46,25 +42,26 @@ if [ "$GIT_SSH_PRIVATE_KEY" != "" ]; then
     mkdir -p "$HOME/.ssh"
     chmod 700 "$HOME/.ssh"
     echo "$GIT_SSH_PRIVATE_KEY" > "$HOME/.ssh/id_rsa"
+
     if [ "$GIT_SSH_PUBLIC_KEY" != "" ]; then
         echo "$GIT_SSH_PUBLIC_KEY" > "$HOME/.ssh/id_rsa.pub"
         chmod 600 "$HOME/.ssh/id_rsa.pub"
     fi
+
     chmod 600 "$HOME/.ssh/id_rsa"
+
     if [ "$GIT_SSH_KNOWN_HOSTS" != "" ]; then
         echo "$GIT_SSH_KNOWN_HOSTS" > "$HOME/.ssh/known_hosts"
         git config --global core.sshCommand "ssh -i $HOME/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=$HOME/.ssh/known_hosts"
+    fi
+
+    if [ "$GIT_SSH_NO_VERIFY_HOST" != "true" ]; then
+        echo >&2 "WARNING: no known_hosts set and host verification is enabled (the default)"
+        echo >&2 "WARNING: this job will fail due to host verification issues"
+        echo >&2 "Please either provide the GIT_SSH_KNOWN_HOSTS or GIT_SSH_NO_VERIFY_HOST inputs"
+        exit 1
     else
-        if [ "$GIT_SSH_NO_VERIFY_HOST" != "true" ]; then
-            tee >&2 <<- 'EOF'
-            WARNING: no known_hosts set and host verification is enabled (the default)
-            WARNING: this job will fail due to host verification issues
-            Please either provide the GIT_SSH_KNOWN_HOSTS or GIT_SSH_NO_VERIFY_HOST inputs
-EOF
-            exit 1
-        else
-            git config --global core.sshCommand "ssh -i $HOME/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-        fi
+        git config --global core.sshCommand "ssh -i $HOME/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
     fi
 else
     git config --global core.askPass /cred-helper.sh
@@ -72,13 +69,14 @@ else
 fi
 
 git remote add "$REMOTE_NAME" "$REMOTE"
+
 if [ "$INPUT_PUSH_ALL_REFS" != "false" ]; then
     eval git push "$GIT_PUSH_ARGS" "$REMOTE_NAME" "\"refs/remotes/origin/*:refs/heads/*\""
+fi
+
+if [ "$HAS_CHECKED_OUT" != "true" ]; then
+    echo >&2 "FATAL: You must upgrade to using actions inputs instead of args: to push a single branch"
+    exit 1
 else
-    if [ "$HAS_CHECKED_OUT" != "true" ]; then
-        echo >&2 "FATAL: You must upgrade to using actions inputs instead of args: to push a single branch"
-        exit 1
-    else
-        eval git push -u "$GIT_PUSH_ARGS" "$REMOTE_NAME" "$GIT_REF"
-    fi
+    eval git push -u "$GIT_PUSH_ARGS" "$REMOTE_NAME" "$GIT_REF"
 fi
